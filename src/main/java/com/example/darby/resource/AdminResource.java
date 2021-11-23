a5a7cd6899c270a1911d814b7ae38b9e1ce4daf4
Leonid Vinogradov <l.vinogradov@hh.ru>
2021-11-23 11:14:38 +0300

    small fix

--- src/main/java/com/example/darby/resource/AdminResource.java
+++ src/main/java/com/example/darby/resource/AdminResource.java
@@ -6,7 +6,6 @@ import com.slack.api.methods.SlackApiException;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +26,7 @@ public class AdminResource {
     this.xoxbToken = xoxbToken;
   }
 
-  @GetMapping("/clear-db")
+  @PostMapping("/clear-db")
   public Mono<Void> getEmployeeById() {
     dao.prepareDatabase();
     return Mono.empty();
